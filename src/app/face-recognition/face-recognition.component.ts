1b733f4976fbe77449778f9e16f73d7ad843adb7
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-09-24 11:35:19 +0300

    Add extract image url & display image

--- src/app/face-recognition/face-recognition.component.ts
+++ src/app/face-recognition/face-recognition.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { FaceRecognitionService } from '../face-recognition.service';
 
 @Component({
-  selector: 'app-face-recognition',
+  selector: 'face-recognition',
   templateUrl: './face-recognition.component.html',
-  styleUrls: ['./face-recognition.component.css']
+  styleUrls: ['./face-recognition.component.css'],
 })
 export class FaceRecognitionComponent implements OnInit {
+  imageLink$: Observable<string>;
 
-  constructor() { }
+  constructor(private faceRecognitionService: FaceRecognitionService) {}
 
   ngOnInit(): void {
+    this.imageLink$ = this.faceRecognitionService.imageLink$;
   }
-
 }
